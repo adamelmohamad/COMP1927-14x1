32bd144233483beeed593d78b2e62f425d943b59
Cyberbully <Cyberbully@users.noreply.github.com>
2014-02-07 19:44:42 +1100

    Update inDegree.c

--- prac2/solutions/inDegree.c
+++ prac2/solutions/inDegree.c
@@ -1,11 +1,11 @@
 int inDegree(Graph g, Vertex v) {
-    if (v >= g->nV) {
+    if (v < 0 || v >= g->nV) {
         return -1;
     }
     int i, count;
     for (i = 0, count = 0; i < g->nV; i++) {
         link curr;
-        for (curr = g->adj[i]; curr != NULL; curr++) {
+        for (curr = g->adj[i]; curr != NULL; curr = curr->next) {
             if (curr->v == v) count++;
         }
     }
